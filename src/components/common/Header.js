e7300f1c1c45d8576fa42ad966076554a78156ad
Luv Mehta <mehluv@gmail.com>
2019-03-17 21:15:31 +0530

    Initial commit with bones of application

---
+++ src/components/common/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../styles/common.css'
+import fIcon from '../../images/fIcon.svg';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div id="header">
+                <div id="header-inset">
+                    <div id="header-logo" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
